0cc3dac789f111db2f19e24c53c9cb1705b832ea
nelsonidehai <nelsonidehai@gmail.com>
2022-07-14 05:26:05 -0700

    A function that prints a buffer

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -14,6 +14,8 @@
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 {
 int index, n1_len = 0, n2_len = 0;
+
+{
 for (index = 0; *(n1 + index); index++)
 n1_len++;
 for (index = 0; *(n2 + index); index++)
@@ -24,4 +26,5 @@ n1 += n1_len - 1;
 n2 += n2_len - 1;
 *(r + size_r) = '\0';
 return (add_strings(n1, n2, r, --size_r));
+}
 }
